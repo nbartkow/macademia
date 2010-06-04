98adce33d0185d57546b33864e9453527b262ef8
nunes.ernesto <nunes.ernesto@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2010-06-04 16:37:34 +0000

    added an institution and the wrapper for the autocomplete

---
+++ Macademia/grails-app/domain/org/macademia/Institution.groovy
@@ -0,0 +1,30 @@
+package org.macademia
+
+class Institution {
+  String name
+  String emailDomain
+
+  static searchable = true
+  static constraints = {
+    name(unique: true)
+  }
+  public String toString() {
+    return "$name"
+  }
+
+  public int compareTo(Object p2) {
+    if (Institution.class.isInstance(p2)) {
+      return emailDomain.compareTo(p2.emailDomain)
+    } else {
+      return -1;
+    }
+  }
+
+  public boolean equals(Object p2) {
+    return (compareTo(p2) == 0)
+  }
+
+  public int hashCode() {
+    return emailDomain.hashCode()
+  }
+}
