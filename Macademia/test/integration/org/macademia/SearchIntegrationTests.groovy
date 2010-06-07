dbb56a7cebef92674d22c4712d87acb676c3bf42
nunes.ernesto <nunes.ernesto@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2010-06-07 16:02:32 +0000

    added search test for institutions and also autocomplete for person, interest and institution

--- Macademia/test/integration/org/macademia/SearchIntegrationTests.groovy
+++ Macademia/test/integration/org/macademia/SearchIntegrationTests.groovy
@@ -3,46 +3,67 @@ package org.macademia
 import grails.test.*
 
 class SearchIntegrationTests extends GrailsUnitTestCase {
-    def searchService
-    def personService
-    def searchableService
-    
-    protected void setUp() {
-        super.setUp()
-        searchableService.reindex()
-    }
+  def searchService
+  def personService
+  def autoCompleteService
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+  protected void setUp() {
+    super.setUp()
+  }
 
-    void testPersonSearch() {
-        def shilad = personService.findByEmail("ssen@macalester.edu")
-        def arjun = personService.findByEmail("guneratne@macalester.edu")
-        def dianna = personService.findByEmail("shandy@macalester.edu")
-        
-        assertNotNull(shilad)
-        assertNotNull(arjun)
-        assertNotNull(dianna)
+  protected void tearDown() {
+    super.tearDown()
+  }
 
-        def people = searchService.searchPeople("shilad")
-        assertEquals(people.size(), 1)
-        assertEquals(people[0], shilad)
-        people = searchService.searchPeople("ssen@macalester.edu")
-        assertEquals(people.size(), 1)
-        assertEquals(people[0], shilad)
-        people = searchService.searchPeople("anthropology")
-        assertEquals(people.size(), 2)
-        assertTrue(people[0].equals(arjun) || people[1].equals(arjun))
-        assertTrue(people[0].equals(dianna) || people[1].equals(dianna))
-    }
+  void testPersonSearch() {
+    def shilad = personService.findByEmail("ssen@macalester.edu")
+    def arjun = personService.findByEmail("guneratne@macalester.edu")
+    def dianna = personService.findByEmail("shandy@macalester.edu")
 
-    void testInterestSearch() {
-        def interests = searchService.searchInterests("foo")
-        assertEquals(interests.size(), 0)
-        interests = searchService.searchInterests("web20")
-        assertEquals(interests.size(), 1)
-        interests = searchService.searchInterests("psychology")
-        assertEquals(interests.size(), 5)
+    assertNotNull(shilad)
+    assertNotNull(arjun)
+    assertNotNull(dianna)
+    autoCompleteService.addPerson(shilad)
+    assertEquals(autoCompleteService.getPersonAutocomplete("sh" , 1).size() , 1)
+    def people = searchService.searchPeople("shilad")
+    assertEquals(people.size(), 1)
+    assertEquals(people[0], shilad)
+    people = searchService.searchPeople("ssen@macalester.edu")
+    assertEquals(people.size(), 1)
+    assertEquals(people[0], shilad)
+    people = searchService.searchPeople("anthropology")
+    assertEquals(people.size(), 2)
+    assertTrue(people[0].equals(arjun) || people[1].equals(arjun))
+    assertTrue(people[0].equals(dianna) || people[1].equals(dianna))
+  }
+
+  void testInterestSearch() {
+    def interests = searchService.searchInterests("foo")
+    assertEquals(interests.size(), 0)
+    interests = searchService.searchInterests("web20")
+    assertEquals(interests.size(), 1)
+    for (Interest interest: interests){
+      autoCompleteService.addInterest(interest)
     }
+    assertEquals(autoCompleteService.getInterestAutocomplete("web" , 2).size() , 1)
+
+    interests = searchService.searchInterests("psychology")
+
+    assertEquals(interests.size(), 5)
+  }
+
+  void testInstitutionSearch() {
+    Institution institution = Institution.findByName("macalester")
+    if(institution == null){
+      institution = new Institution(name: "macalester", emailDomain: "@macalester.edu")
+      institution.save()
+    }
+    def institutions = searchService.searchInstitutions("macalester")
+    assertEquals(institutions.size(), 1)
+    autoCompleteService.addInstitution(institution)
+    assertEquals(autoCompleteService.getInstitutionAutocomplete("mac",2).size() , 1)
+
+  }
+
+
 }
