5d188265332678ea0c6bd2b1b60246f44392c43b
nunes.ernesto <nunes.ernesto@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2010-06-07 16:01:06 +0000

    fixed few bugs

--- trunk/Macademia/grails-app/services/org/macademia/AutoCompleteService.groovy
+++ trunk/Macademia/grails-app/services/org/macademia/AutoCompleteService.groovy
@@ -16,7 +16,7 @@ class AutoCompleteService {
   static int MAX_NUMBER_RES = 10
   AutocompleteTree<String, Person> personTree = new AutocompleteTree<String, Person>()
   AutocompleteTree<String,Institution> institutionTree = new  AutocompleteTree<String,Institution>()
-  AutocompleteTree<Interest,Interest> interestTree = new  AutocompleteTree<Interest,Interest>()
+  AutocompleteTree<String,Interest> interestTree = new  AutocompleteTree<Interest,Interest>()
 
   // creates a tree for person class
   def addPerson(Person person){
@@ -31,20 +31,46 @@ class AutoCompleteService {
   }
 
   def addInterest(Interest interest){
-    interestTree.add(interest, interest)
+    interestTree.add(interest.hashCode(), interest)
 
   }
 
   def getPersonAutocomplete(String query,int MAX_NUMBER_RES){
     // Returns the top three cities that start with "ch" ordered by score.
     List<Person> people = new ArrayList<Person> ()
-    SortedSet<AutocompleteEntry<String, Person>> results = personTree.autocomplete(query, MAX_NUMBER_RES);
+    SortedSet<AutocompleteEntry<String, Person>> results = personTree.autocomplete(query, MAX_NUMBER_RES)
     for (AutocompleteEntry<String, Person> entry : results) {
       people.add((Person)entry.getValue())
-      System.out.println("Person " + entry.getValue() + " with score " + entry.getScore());
+      System.out.println("Person " + entry.getValue() + " with score " + entry.getScore())
     }
 
     return people
+
+  }
+
+  def getInstitutionAutocomplete(String query,int MAX_NUMBER_RES){
+    // Returns the top three cities that start with "ch" ordered by score.
+    List<Institution> institutions = new ArrayList<Institution> ()
+    SortedSet<AutocompleteEntry<String, Institution>> results = institutionTree.autocomplete(query, MAX_NUMBER_RES)
+    for (AutocompleteEntry<String, Institution> entry : results) {
+      institutions.add((Institution)entry.getValue())
+      System.out.println("Institution " + entry.getValue() + " with score " + entry.getScore())
+    }
+
+    return institutions
+
+  }
+
+  def getInterestAutocomplete(String query,int MAX_NUMBER_RES){
+    // Returns the top three cities that start with "ch" ordered by score.
+    List<Interest> interests = new ArrayList<Interest> ()
+    SortedSet<AutocompleteEntry<String, Interest>> results = interestTree.autocomplete(query, MAX_NUMBER_RES)
+    for (AutocompleteEntry<String, Interest> entry : results) {
+      interests.add((Interest)entry.getValue())
+      System.out.println("Interest " + entry.getValue() + " with score " + entry.getScore())
+    }
+
+    return interests
 
   }
 }
