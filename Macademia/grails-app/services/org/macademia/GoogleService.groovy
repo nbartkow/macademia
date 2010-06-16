3befdb11da402b2fc1485d0256d57053a8a9cf29
nathaniel.miller.20 <nathaniel.miller.20@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2010-06-16 14:37:59 +0000

    bug fixes

--- Macademia/grails-app/services/org/macademia/GoogleService.groovy
+++ Macademia/grails-app/services/org/macademia/GoogleService.groovy
@@ -4,14 +4,25 @@ import org.json.JSONException
 
 class GoogleService {
 
+    static File googleCache = null
     static transactional = false
     ThreadLocal<Google> holder = new ThreadLocal<Google>()
 
     public List<String> query (String query, int maxResults) {
         if (holder.get() == null) {
-            holder.set(new Google())
+            if (googleCache != null) {
+                holder.set(new Google(googleCache))
+            }   else {
+                holder.set(new Google())
+            }
         }
         return holder.get().query(query, maxResults)
+    }
+
+    public void setCache(File cache){
+        googleCache=cache
+        Google google = new Google(cache)
+        holder.set(google)
     }
 
 }
