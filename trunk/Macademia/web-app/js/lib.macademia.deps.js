011b29fbcf648c0ff7d3ee1a3968c5e260b05c63
hwcharlton <hwcharlton@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2010-07-09 20:02:14 +0000

    added some jquery libraries and stuff

---
+++ trunk/Macademia/web-app/js/lib.macademia.deps.js
@@ -0,0 +1,16 @@
+var NIMBLE_BASE = "/plugins/nimble-0.4-SNAPSHOT/dev/";
+
+$.deps.init('/Macademia', {
+    'nimble-login-register': [
+        NIMBLE_BASE + "/js/jquery/jquery.url.js",
+        NIMBLE_BASE + "/js/jquery/jquery.bt.custom.js",
+        NIMBLE_BASE + "/js/jquery/jquery.jgrowl.js",
+        NIMBLE_BASE + "/js/jquery/nimblecore.js",
+        NIMBLE_BASE + "/js/jquery/nimbleui.js",
+        NIMBLE_BASE + "/js/jquery/nimbleui.growl.js",
+        NIMBLE_BASE + "/js/jquery/jquery.pstrength.min.js",
+        NIMBLE_BASE + "/css/famfamfam.css",
+        NIMBLE_BASE + "/css/administration.css",
+        NIMBLE_BASE + "/css/jquery/jgrowl.css"
+    ]
+});
