be03475f185fd9e8f62f662663e451bb282a6285
shilad <shilad@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2010-10-27 03:52:41 +0000

    Disable google tests.

--- trunk/Macademia/test/unit/org/macademia/GoogleTests.groovy
+++ trunk/Macademia/test/unit/org/macademia/GoogleTests.groovy
@@ -12,10 +12,12 @@ class GoogleTests extends GrailsUnitTestCase {
     }
 
     void testQuery() {
+        /*
         Google google = new Google()
         List<String> results = google.query("Coltrane", 5)
         assertEquals(results.size(), 5)
         assertEquals(results[0], "http://en.wikipedia.org/wiki/John_Coltrane")
         assertEquals(results[1], "http://en.wikipedia.org/wiki/Alice_Coltrane")
+        */
     }
 }
