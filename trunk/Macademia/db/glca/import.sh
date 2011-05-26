013d679c28173996e798141e2334e4a11e09bb71
shilad <shilad@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2011-05-26 19:59:21 +0000

    Added import scripts for GLCA

---
+++ trunk/Macademia/db/glca/import.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python ./cleanup.py GLCA\ Interests\ Complete.txt GLCA-Interests-Complete-Cleaned.txt &&
+(cd ../.. &&
+grails prod run-script ./scripts/LoadPeople.groovy < ./db/glca/GLCA-Interests-Complete-Cleaned.txt
+)
+#grails prod run-script ./db/glca/create_schools.groovy &&
