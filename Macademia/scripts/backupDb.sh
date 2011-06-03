ac8c702acc8c829038c8f4a26591fda1c27e9213
JeremyRLim <jeremyrlim@gmail.com>
2011-06-03 16:48:52 +0000

    Updated backup script to include WikipediaReadOnlyTest

--- Macademia/scripts/backupDb.sh
+++ Macademia/scripts/backupDb.sh
@@ -5,18 +5,27 @@ export PATH=/Library/PostgreSQL/8.4/bin/:/Users/shilad/Downloads/mongodb-osx-x86
 
 MONGO_BACKUP=./db/prod.db.mongo
 MONGO_WP_BACKUP=./db/prod.db.mongo.wp
+MONGO_WP_BACKUP_TEST=./db/prod.db.mongo.wp.test
 PSQL_BACKUP=./db/prod.db.psql
 WWW_DIR=/data/shilad/www/data
 
 if ! [ -d $MONGO_BACKUP ]; then
     mkdir $MONGO_BACKUP
 fi
+if ! [ -d $MONGO_WP_BACKUP ]; then
+    mkdir MONGO_WP_BACKUP
+fi
+if ! [ -d MONGO_WP_BACKUP_TEST ]; then
+    mkdir MONGO_WP_BACKUP_TEST
+fi
 
-#grails extract-prod-wp &&
+grails extract-prod-wp &&
 pg_dump -U grails macademia_prod -f $PSQL_BACKUP && \
 mongodump --db macademia_prod -o $MONGO_BACKUP && \
 mongodump --db wikipediaReadOnlySmall -o $MONGO_WP_BACKUP && \
+mongodump --db wikipediaReadOnlyTest -o MONGO_WP_BACKUP_TEST && \
 tar -cpz $PSQL_BACKUP >$WWW_DIR/`basename $PSQL_BACKUP`.tar.z && \
 tar -cpz $MONGO_BACKUP >$WWW_DIR/`basename $MONGO_BACKUP`.tar.z && \
-tar -cpz $MONGO_WP_BACKUP >$WWW_DIR/`basename $MONGO_WP_BACKUP`.tar.z ||
+tar -cpz $MONGO_WP_BACKUP >$WWW_DIR/`basename $MONGO_WP_BACKUP`.tar.z && \
+tar -cpz MONGO_WP_BACKUP_TEST >$WWW_DIR/`basename MONGO_WP_BACKUP_TEST`.tar.z ||
     { echo "backup failed!">&2; exit 1; }
