b5134e255e4ec489b6a6ba33275fb9594fa4ee6b
nicholas.p.brooks@gmail.com <nicholas.p.brooks@gmail.com@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2011-06-06 18:37:46 +0000

    Reconfig Autocomplete to show person's college affiliation and update autocomplete tree when user changes their personal information

--- trunk/Macademia/src/java/org/macademia/AutocompleteEntity.java
+++ trunk/Macademia/src/java/org/macademia/AutocompleteEntity.java
@@ -8,15 +8,28 @@ public class AutocompleteEntity {
     private long id;
     private String name;
     private Class klass;
+    private String other;
 
     public AutocompleteEntity(long id, String name, Class klass) {
         this.id = id;
         this.name = name;
         this.klass = klass;
+        this.other = "";
+    }
+
+    public AutocompleteEntity(long id, String name, Class klass, String other) {
+        this.id = id;
+        this.name = name;
+        this.klass = klass;
+        this.other = other;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getOther() {
+        return other;
     }
 
     public long getId() {
@@ -25,6 +38,10 @@ public class AutocompleteEntity {
 
     public Class getKlass() {
         return klass;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
     }
 
     @Override
