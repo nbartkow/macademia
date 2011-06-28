36c637febd498edb333aa15d89b0418f4e0e576a
aaronlaursen@gmail.com <aaronlaursen@gmail.com@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2011-06-28 15:52:26 +0000

    encrypt db dump

--- trunk/Macademia/scripts/backupDb.sh
+++ trunk/Macademia/scripts/backupDb.sh
@@ -9,6 +9,13 @@ MONGO_WP_BACKUP_TEST=./db/prod.db.mongo.wp.test
 PSQL_BACKUP=./db/prod.db.psql
 WWW_DIR=/data/shilad/www/data
 
+KEYFILE=./scripts/.dumpkey
+
+if ! [ -z $KEYFILE ]; then
+    echo "No key found."
+    exit 1
+fi
+
 if ! [ -d $MONGO_BACKUP ]; then
     mkdir $MONGO_BACKUP
 fi
@@ -24,8 +31,8 @@ pg_dump -U grails macademia_prod -f $PSQL_BACKUP && \
 mongodump --db macademia_prod -o $MONGO_BACKUP && \
 mongodump --db wikipediaReadOnlySmall -o $MONGO_WP_BACKUP && \
 mongodump --db wikipediaReadOnlyTest -o $MONGO_WP_BACKUP_TEST && \
-tar -cpz $PSQL_BACKUP >$WWW_DIR/`basename $PSQL_BACKUP`.tar.z && \
-tar -cpz $MONGO_BACKUP >$WWW_DIR/`basename $MONGO_BACKUP`.tar.z && \
-tar -cpz $MONGO_WP_BACKUP >$WWW_DIR/`basename $MONGO_WP_BACKUP`.tar.z && \
-tar -cpz $MONGO_WP_BACKUP_TEST >$WWW_DIR/`basename $MONGO_WP_BACKUP_TEST`.tar.z ||
+tar -cpz $PSQL_BACKUP | openssl aes-256-cbc -pass file:$KEYFILE >$WWW_DIR/`basename $PSQL_BACKUP`.tar.z && \
+tar -cpz $MONGO_BACKUP | openssl aes-256-cbc -pass file:$KEYFILE >$WWW_DIR/`basename $MONGO_BACKUP`.tar.z && \
+tar -cpz $MONGO_WP_BACKUP | openssl aes-256-cbc -pass file:$KEYFILE >$WWW_DIR/`basename $MONGO_WP_BACKUP`.tar.z && \
+tar -cpz $MONGO_WP_BACKUP_TEST | openssl aes-256-cbc -pass file:$KEYFILE >$WWW_DIR/`basename $MONGO_WP_BACKUP_TEST`.tar.z ||
     { echo "backup failed!">&2; exit 1; }
