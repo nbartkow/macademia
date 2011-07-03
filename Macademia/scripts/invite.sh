8c608d6040172b7b681d9f0f8fef182a9dc94849
shilad <shilad@22e0cbe0-2fb8-11df-a735-6ff41f51f3aa>
2011-07-03 05:31:09 +0000

    Updated invitation script

--- Macademia/scripts/invite.sh
+++ Macademia/scripts/invite.sh
@@ -1,6 +1,24 @@
+if ! [ -f "$1" ]; then
+    echo "usage: $0 emails_file email_template" >& 2
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "usage: $0 emails_file email_template" >& 2
+    exit 1
+fi
+
+EMAIL_FILE=$1
+EMAIL_TEMPLATE=$2
+ADMIN_URL=http://localhost:8080/Macademia/all/administrator
+
+wget -O - "$ADMIN_URL/resetKey"
+
+echo -n "enter new auth key from log file: "
+read key
+
 while read email; do
     echo "inviting $email"
-    wget -O - "http://localhost:10090/Macademia/person/reinvite?email=$email"
+    wget -O - "$ADMIN_URL/invite?key=$key&email=$email&template=$EMAIL_TEMPLATE"
     echo
     echo
-done
+done <$EMAIL_FILE
