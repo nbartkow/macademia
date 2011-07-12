8b6fe15b10780d545378f5d599e32a3ba074a82c
shilad <shilad@gmail.com>
2011-07-12 20:09:17 +0000

    Added "add institution" for createinstitutiongroup script.

--- Macademia/scripts/CreateInstitutionGroup.groovy
+++ Macademia/scripts/CreateInstitutionGroup.groovy
@@ -3,107 +3,131 @@ import org.macademia.*
 InstitutionGroup.withTransaction {
     println('')
     while ( true ) {
-        InstitutionGroup ig1 = checkSelection( readSelectionCode() )
-        String inputs = new String()
-        if( !ig1 ) {
+        int i = readSelectionCode()
+        if (i == 1) {
+            createInstitution()
+        } else if (i == 2) {
+            createInstitutionGroup()
+        } else if (i == 3) {
+            addInstitutionsToGroup()
+        } else if (i == 4) {
             println('Run Complete\n')
-            break // Exit Main loop
+            break;
+        } else {
+            println("Invalid selection: $i\n")
         }
-        while( true ) {
-            inputs = readText( "Enter name/domain of institution to add or 'end' " )
-            if( inputs == 'end') {
-                break
-            }
-            Institution school = Institution.findByEmailDomain( inputs )
-            if ( !school ) {
-                school = Institution.findByName( inputs )
-            }
-            if( !school ) {
-                println("Error: No such institution found in database!")
-            } else if(!ig1.institutions || !ig1?.institutions?.contains(school)){
-                ig1.addToInstitutions( school )
-                school.addToInstitutionGroups( ig1 )
-                Utils.safeSave( school )
-            } else {
-                println("Institution '${school.name}' is already a member of group '${ig1.name}'!")
-            }
-        }
-        Utils.safeSave( ig1 )
-        System.console().flush()
     }
 }
 
 int readSelectionCode() {
-    int responseCode = stringToInt( readText(
+     return readText(
         """
-            Institution Group Management\n
-            Choose an option from the menu:\n
-            \t1) Create a new InstitutionGroup\n
-            \t2) Add an Institution to an existing InstitutionGroup\n
-            \t3) End
-            Enter number of selection:
-        """) )
-    return responseCode
+Institution Group Management
+Choose an option from the menu:
+\t1) Create a new Institution
+\t2) Create a new InstitutionGroup
+\t3) Add an Institution to an existing InstitutionGroup
+\t4) End
+Enter number of selection:
+        """) as int
 }
 
-InstitutionGroup checkSelection( int selectionCode ) {
-    InstitutionGroup ig1 = null
-    while( true ) {
-        def igText = []
-
-        if( selectionCode == 1){
-            // Handles case of creating a new IG. Checks that IG does not already exist
-            String igName, igAbbrev
-
-            while( (igText as List<String>).size() != 2 ) {
-                if( !(igText as List<String>).isEmpty() ) {
-                    println("Error: Response must in the form 'Group, abbreviation' ")
-                }
-                igText = readText( "Enter 'Name' of new InstitutionGroup and group 'Abbreviation', separated by a comma: " ).split(',')
-            }
-            igName = igText[0]?.trim()
-            igAbbrev = igText[1].trim().toLowerCase()
-
-            if( !InstitutionGroup.findByAbbrev( igAbbrev ) ) {
-                ig1 = new InstitutionGroup(name : igName, abbrev : igAbbrev )
-                break
-            } else {
-                selectionCode = stringToInt( readText("\nInstitutionGroup with Abbrev $igAbbrev already exists. Choose another menu option: ") )
-                igText=[]
-            }
-        } else if ( selectionCode == 2 ) {
-            // Handles case of adding an Institution to an existing IG. Checks if IG exists
-            String igAbbrev = readText( "Enter group 'Abbreviation': " )
-            ig1 = InstitutionGroup.findByAbbrev( igAbbrev )
-
-            if(!ig1) {
-                selectionCode = stringToInt( readText("\nInstitutionGroup with Abbrev $igAbbrev does not exist. Choose another option: ") )
-            } else {
-                break
-            }
-        } else if ( selectionCode == 3 ) {
-            // User selects 'End'. Break out of while loop and return 'escape = true' to main method
-            escape = true
-            break
+void createInstitution() {
+    def iText
+    while(true) {
+        iText = readText("""
+Enter 'name, url, type, abbreviation, email domain'
+Where name, url, and type are required
+And type can be 'school' or 'group'.
+        """)
+        iText = iText.split(',').collect({it.trim() ? it.trim() : null})
+        if (iText.size() != 5) {
+            println("incorrect number of fields specified")
+        } else if (!iText[0] || !iText[1] || !iText[2]) {
+            println("first three fields are required")
+        } else if (!['school', 'group'].contains(iText[2])) {
+            println("invalid type field")
         } else {
-            // Handle typos/invalid entries
-            selectionCode = stringToInt( readText('Invalid option. Enter number of selection: ') )
+            break
+        }
+        println("Error in input format.")
+    }
+    def (iName, iWebUrl, iType, iAbbrev, iEmailDomain) = iText
+    if (iType == 'school') {
+        iType = Institution.TYPE_SCHOOL
+    } else if (iType == 'group') {
+        iType = Institution.TYPE_GROUP
+    } else {
+        throw new RuntimeException("Should never happen: ${iType}")
+    }
+
+    if( !Institution.findByWebUrl( iWebUrl ) ) {
+        Institution i = new Institution(name : iName, webUrl: iWebUrl, abbrev: iAbbrev, type : iType, emailDomain : iEmailDomain)
+        i.memberships = [] as Set
+        InstitutionGroup all = InstitutionGroup.findByAbbrev('all')
+        if (!all) {
+            throw new IllegalStateException("all institution group does not exist")
+        }
+        all.addToInstitutions(i)
+        i.addToInstitutionGroups(all)
+        Utils.safeSave(i)
+        Utils.safeSave(all)
+    } else {
+        println("\nInstitutionGroup with url $iWebUrl already exists. Choose another menu option: ")
+    }
+}
+
+void createInstitutionGroup() {
+    def igText
+    while (true) {
+        igText = readText( "Enter 'Name' of new InstitutionGroup and group 'Abbreviation', separated by a comma: " ).split(',')
+        if (igText.size() == 2 ) {
+            break
+        }
+        println("Error: Response must in the form 'Group, abbreviation' ")
+    }
+    def (igName, igAbbrev) = igText.collect({it.trim() ? it.trim() : null})
+    igAbbrev = igAbbrev.toLowerCase()
+
+    if( !InstitutionGroup.findByAbbrev( igAbbrev ) ) {
+        ig = new InstitutionGroup(name : igName, abbrev : igAbbrev )
+        Utils.safeSave(ig)
+    } else {
+        println("\nInstitutionGroup with Abbrev $igAbbrev already exists. Choose another menu option: ")
+    }
+}
+
+void addInstitutionsToGroup() {
+    // Handles case of adding an Institution to an existing IG. Checks if IG exists
+    String igAbbrev = readText( "Enter group 'Abbreviation': " )
+    ig = InstitutionGroup.findByAbbrev( igAbbrev )
+    if (!ig) {
+        println("no group with institution ${igAbbrev}")
+        return
+    }
+    while( true ) {
+        inputs = readText( "Enter name/domain of institution to add or 'end' " )
+        if( inputs == 'end') {
+            break
+        }
+        Institution school = Institution.findByEmailDomain( inputs )
+        if ( !school ) {
+            school = Institution.findByName( inputs )
+        }
+        if( !school ) {
+            println("Error: No such institution found in database!")
+        } else if(!ig.institutions || !ig.institutions.contains(school)){
+            ig.addToInstitutions( school )
+            school.addToInstitutionGroups(ig)
+            Utils.safeSave( school )
+        } else {
+            println("Institution '${school.name}' is already a member of group '${ig.name}'!")
         }
     }
-    return ig1
+    Utils.safeSave(ig)
 }
 
 String readText(String prompt) {
-    println( prompt );
+    println( prompt )
     return System.console().readLine()
-}
-
-int stringToInt(String string) {
-    try {
-        int num = string.toInteger()
-        return num
-    }
-    catch(NumberFormatException) {
-        return 0
-    }
 }
